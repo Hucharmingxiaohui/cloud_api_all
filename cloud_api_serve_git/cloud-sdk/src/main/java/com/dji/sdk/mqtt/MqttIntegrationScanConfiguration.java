a825541db403ec45563903f36f88c68beee7eab2
hu <908288323@qq.com>
2026-07-03 15:54:37 +0800

    补充

---
+++ cloud_api_serve_git/cloud-sdk/src/main/java/com/dji/sdk/mqtt/MqttIntegrationScanConfiguration.java
@@ -0,0 +1,9 @@
+package com.dji.sdk.mqtt;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+
+@Configuration
+@IntegrationComponentScan
+public class MqttIntegrationScanConfiguration {
+}
